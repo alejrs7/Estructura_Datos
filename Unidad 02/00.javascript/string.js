ade9e57e915c02e4d353519819153a8e98d947da
David Ruiz <david.a.ruiz@unl.edu.ec>
2026-06-12 13:12:01 -0500

    Corregido error variable phrase y uso de comillas simples en string.js

---
+++ Unidad 02/00.javascript/string.js
@@ -0,0 +1,15 @@
+let string1 = 'Hello, world';
+let string2 = "JavaScript's chevere!";
+let string3 = 'JavaScript es chevere';
+let string4 = string1 + ' ' + string2;
+
+console.log(string1);
+console.log(string2);
+console.log(string3);
+console.log(string4);
+
+let phrase = "JavaScript is Great";
+console.log(phrase.length);
+console.log(phrase.toLowerCase());
+console.log(phrase.toUpperCase());
+console.log(phrase.substring(0, 10));
---
+++ Unidad 02/00.javascript/tipos-datos.js
@@ -0,0 +1,28 @@
+//Primitivos
+let name =David
+
+//Numericos
+let age =21
+
+//Boleeanos
+let esMayorDeEdad = true
+
+//null
+let noHayvalor = null
+
+Undefined
+let noDefined = undefined
+//symbol unique value
+let symbolUnico = Sumbol("unique")
+
+//Full
+Object
+let cart = {
+    "carro": "Valor",   
+    marca: 'Tesla',     
+    modelo: 'Model',     
+}
+//FUNCTIONS 
+function salute(){
+    
+}
