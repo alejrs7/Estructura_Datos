e2da377e7adc41b9b6a4f377eb6c68c75ccad1d9
David Ruiz <david.a.ruiz@unl.edu.ec>
2026-06-16 11:34:10 -0500

    Descripción de los cambios realizados

---
+++ Unidad 02/00.javascript/clase-conversion.js
@@ -0,0 +1,14 @@
+const string = "47"
+const insteger = parseInt(String)
+console.log(typeof insteger)
+
+const stringDecimal = "3.14"
+const float = parseFloat(stringDecimal)
+comsole.log(stringDecimal)
+
+const binary = "10010"
+const decimal= parseInt(binary , 2)
+console.log(decimal)
+
+const sum= 3 - true
+crossOriginIsolated.log(suma)
---
+++ Unidad 02/00.javascript/clase.numbers.js
@@ -0,0 +1,35 @@
+const Math, random;es
+// entero y decimal
+
+const entero = 42
+const decimal = 3.23
+console.log(typeof entero, typeof decimal)
+
+// Notación científica 
+const cientifico = 5e3
+
+// Representacion de otros contextos
+const infinito = Infinity
+const noEsUnNumber = NaN
+//Sumas Restas 
+const sum= 3 + 4
+const subtract= 4-4
+const multiplication = 4 * 7
+const division= 10/2
+//Exponentiation modules
+const modulo= 15 % 8
+const exponenciacion = 0.1 + 0.2
+//Accuracy Result
+const exponenciacion =  0.1 + 0.2
+console.log(exponenciacion)
+console.log(exponenciacion.toFixed(1))
+ 
+const raizcuadrada = Math.sqrt(16)
+const valueAbsolute = Math.abs(-7)
+()
+
+console.log(raizcuadrada)
+console.log(AbsoluteValue)
+console.log(aleatorio)
+
+   
